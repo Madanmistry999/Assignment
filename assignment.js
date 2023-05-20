3e84f629ac9860bd99ca8ef9bf8fe40d1cdc5386
Madan <madanmistry999@gmail.com>
2023-05-20 10:28:28 +0530

    updated assignment.js file

--- assignment.js
+++ assignment.js
@@ -1,11 +1,11 @@
+var a=20;
+var b=20;
+
 function addition(num1,num2){
 
     var sum=num1+num2;
 
-    return document.getElementById('disp').innerHTML="Sum of two number is:"+sum;
+    return console.log("Sum of two number is:"+sum);
 }
-
-var a=document.getElementById('num1');
-var b=document.getElementById('num2');
 
 addition(a,b);
