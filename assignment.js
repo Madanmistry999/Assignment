14627d317f3070825d248cac611d50f86bc4c843
Madan <madanmistry999@gmail.com>
2023-05-20 10:09:15 +0530

    delete launch file

---
+++ assignment.js
@@ -0,0 +1,11 @@
+function addition(num1,num2){
+
+    var sum=num1+num2;
+
+    return console.log("Sum of two number is:"+sum);
+}
+
+var a=Number(prompt("Enter num1:"));
+var b=Number(prompt("Enter num2:"));
+
+addition(a,b);
